0e47e2ffe32d803fe59f690b16ea99a7708a609c
Ben Hyde <bhyde@pobox.com>
2013-03-17 20:31:00 -0400

    It has been brought to my attention that "If :use is not supplied, it defaults to the same implementation-dependent value" and that SBCL's defaults do not inclued the common-lisp package.

--- packages.lisp
+++ packages.lisp
@@ -2,12 +2,14 @@
 
 (defpackage "CL-HOTP"
   (:nicknames "HOTP")
+  (:use "COMMON-LISP")
   (:export "*DIGITS*"
            "*HMAC-SHA-MODE*"
            "HOTP"))
 
 (defpackage "CL-TOTP"
   (:nicknames "TOTP")
+  (:use "COMMON-LISP")
   (:export "*TIME-ZERO*"
            "*TIME-STEP-IN-SECONDS*" 
            "TOTP"))
